26a65bffeb4d3dd28a70745ceef3a0591a59f140
Caterina <ccnn1010@gmail.com>
2020-05-19 02:07:04 +1000

    nodemail added to dependicies and conf deleted

--- app.js
+++ app.js
@@ -1,23 +1,62 @@
 const express = require('express');
 const chalk = require('chalk');
 require('dotenv').config()
-const database = require('./conf'); 
 const bodyParser = require('body-parser')
-
 const app = express();
-
+const nodemailer = require('nodemailer')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json()) 
 
 
 
+//connects to host domain
+let transporter = nodemailer.createTransport({
+  service: 'gmail',
+//connect to gmail
+  auth: {
+    user: 'process.env.EMAIL',
+    pass: 'process.env.PASSWORD'
+  }
+});
+
+//create mail options (what is sent to user)
+
+let mailOptions = {
+  from: 'nncc11911@gmail.com',
+  to: 'ccnn1010@gmail.com',
+  subject: 'Test',
+  text: 'It is working'
+}
+
+
+
+//take variable transporter to create a function to send the email
+transporter.sendMail(mailOptions, function (err, data){
+  if (err) {
+    console.log('Error has occurred', err);
+  } else {
+    console.log('Email has been sent');
+  }
+});
+
+
+/*  transporter.sendMail(mailOptions)
+  .then(function(response){
+    console.log('Email sent');
+  })
+  .catch(function(error) {
+    console.log('Error: ', error);
+  });
+ */
+
+
 //GET route to home page
-app.get('/', (req, res)=>{
+/* app.get('/', (req, res)=>{
     res.send('Connected')
-})
+}) */
 
 //Route to connect checkpoint database /playlist and /track
- app.get('/contacts', (req, res)=>{
+/*  app.get('/contacts', (req, res)=>{
     database.query('SELECT * FROM contact', (error, results)=>{
         if(error){ 
             console.log(chalk.red.inverse('There is an error with GET /contact'));
@@ -28,13 +67,13 @@ app.get('/', (req, res)=>{
         }
     })
 }) 
-
+ */
 
 
 //1.  POST - create a new playlist 
 //201 status code - created
 //400 status code - Bad Request
-app.post('/newContact', (req, res)=>{
+/* app.post('/newContact', (req, res)=>{
     database.query('INSERT INTO contact SET ?', req.body, (error, results)=>{
       if(!error){
         res.status(201).json(req.body);
@@ -43,7 +82,7 @@ app.post('/newContact', (req, res)=>{
       }
     })
   })
-
+ */
 
 
 
